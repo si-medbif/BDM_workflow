07a6f58c2acc9bbadf3c3a8968bc2beff8c6bc40
Harald Grove <haraldgrove@gmail.com>
2020-03-24 15:05:35 +0700

    Added optional input.

--- RNA/count2tpm.py
+++ RNA/count2tpm.py
@@ -2,8 +2,10 @@
 
 """
     Collects output from single sample featureCounts analysis.
-    Input:  Folder name where featureCounts output is stored
-            Output files should be named as <SAMPLE>_readcounts.featurecounts.txt
+    Input:  Either: 
+              Folder name where featureCounts output is stored as <SAMPLE>_readcounts.featurecounts.txt
+            Or:
+              File with list of featureCounts output files. First line is folder to store output.
     Output: One file with TPM values for each sample
             One file with raw counts for each sample
 """
@@ -12,14 +14,32 @@ import sys
 import glob
 import pandas as pd
 
-infolder = sys.argv[1]
+inparam = sys.argv[1]
 
-outfile1 = infolder+'/all-samples_tpm.txt'
-outfile2 = infolder+'/all-samples_raw.txt'
+try:
+    with open(inparam, 'r') as fin:
+        files = []
+        for line in fin:
+            l = line.strip().split()
+            if len(l) == 2:
+                if l[0] == 'PROJECT':
+                    project_name = l[1]
+                elif l[0] == 'OUT':
+                    out_folder = l[1]
+                elif l[0] == 'FILE':
+                    files.append(l[1])
+            else:
+                pass
+except IsADirectoryError:
+    files = glob.glob(inparam+'/*readcounts.featurecounts.txt')
+    out_folder = inparam
+    project_name = 'project'
 
-files = glob.glob(infolder+'/*readcounts.featurecounts.txt')
+outfile1 = '{}/{}_{}-samples_tpm.tsv'.format(out_folder, project_name, len(files))
+outfile2 = '{}/{}_{}-samples_raw.tsv'.format(out_folder, project_name, len(files))
 
 for findex, infile in enumerate(files):
+    infile.strip().rsplit('_',1)[0] + 'counts.tsv'
     sample = infile.split('/')[-1].rsplit('_',1)[0]
     indf = pd.read_csv(infile, header=0, sep='\t', comment='#')
     if findex == 0:
@@ -30,8 +50,8 @@ for findex, infile in enumerate(files):
     columns.append(sample)
     rawdf = pd.concat([rawdf, df1], axis=1)
 rawdf.columns = columns
-
-rawdf.to_csv(outfile2, sep='\t', index=False)
+outdf = rawdf.drop('Length', axis=1)
+outdf.to_csv(outfile2, sep='\t', index=False, float_format='%.3f')
 tpmdf = rawdf.iloc[:,[0, 1]]
 
 for col_ind in range(2, len(files)+2):
@@ -40,5 +60,7 @@ for col_ind in range(2, len(files)+2):
     tpmdf = pd.concat([tpmdf, df1], axis=1)
 
 tpmdf.columns = columns
-tpmdf.to_csv(outfile1, sep='\t', index=False)
+
+outdf = tpmdf.drop('Length', axis=1)
+outdf.to_csv(outfile1, sep='\t', index=False, float_format='%.3f)
 
