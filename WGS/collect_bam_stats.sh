0401e4f488aa2120e699418b54068eb01c6f3dcc
Harald Grove <haraldgrove@gmail.com>
2020-05-29 15:23:58 +0700

    WGS reports

---
+++ WGS/collect_bam_stats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# In version 1.9: -p will remove overlaps of pair-end reads from counting
+BAMFOLDER=$1
+SAMPLE=$2
+samtools stats \
+	-p \
+	--threads 8 \
+	${BAMFOLDER}/${SAMPLE}/BAM/${SAMPLE}_dedupped.bam \
+        > ${BAMFOLDER}/${SAMPLE}/BAM/${SAMPLE}_dedupped.bam.stats.txt
