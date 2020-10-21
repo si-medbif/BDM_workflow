ce3bbb63775b9690c61be1ebdb22e59d9efb29c8
Harald Grove <haraldgrove@gmail.com>
2020-10-21 10:23:02 +0700

    count2tpm accepts multi sample featurecounts input

--- RNA/count2tpm.py
+++ RNA/count2tpm.py
@@ -20,6 +20,7 @@ ap = argparse.ArgumentParser()
 group = ap.add_mutually_exclusive_group()
 group.add_argument("-f", "--folder", help="path to the folder with count-files")
 group.add_argument("-s", "--samples", help="file with list of the count-files")
+group.add_argument("-m", "--multi", help="Multi-sample file")
 ap.add_argument("-n", "--names", help="Map for gene translation [alias1 alias2 ... new]")
 ap.add_argument("-o", "--outfile", help="Output file prefix", default="project")
 args = vars(ap.parse_args())
@@ -33,8 +34,10 @@ elif args['folder'] is not None:
     files = glob.glob(args["folder"]+'/*readcounts.featurecounts.txt')
     out_folder = args["folder"]
     project_name = 'project'
+elif args['multi'] is not None:
+    pass
 else:
-    sys.stderr.write('ERROR: missing sample list / sample folder\n')
+    sys.stderr.write('ERROR: missing or incorrect input.\n')
     sys.exit(1)
 
 db = {}
@@ -45,28 +48,38 @@ if args["names"] is not None:
             for el in l[0:-1]:
                 db[el] = l[-1]
 
-outfile1 = '{}_{}-samples_tpm.tsv'.format(args["outfile"], len(files))
-outfile2 = '{}_{}-samples_raw.tsv'.format(args["outfile"], len(files))
+if args['multi'] is not None:
+    indf = pd.read_csv(args['multi'], header=0, sep='\t', comment='#')
+    rawdf = indf.drop(['Chr','Start','End','Strand'], axis=1)
+    num_samples = len(rawdf.columns) - 2
+else:
+    num_samples = len(files)
+    for findex, infile in enumerate(files):
+        infile.strip().rsplit('_',1)[0] + 'counts.tsv'
+        sample = infile.split('/')[-1].rsplit('_',1)[0]
+        indf = pd.read_csv(infile, header=0, sep='\t', comment='#')
+        if findex == 0:
+            rawdf = indf.iloc[:,[0, 5, 6]]
+            columns = ['Geneid', 'Length', sample]
+            continue
+        df1 = indf.iloc[:,6]
+        columns.append(sample)
+        rawdf = pd.concat([rawdf, df1], axis=1)
+        rawdf.columns = columns
 
-for findex, infile in enumerate(files):
-    infile.strip().rsplit('_',1)[0] + 'counts.tsv'
-    sample = infile.split('/')[-1].rsplit('_',1)[0]
-    indf = pd.read_csv(infile, header=0, sep='\t', comment='#')
-    if findex == 0:
-        rawdf = indf.iloc[:,[0, 5, 6]]
-        columns = ['Geneid', 'Length', sample]
-        continue
-    df1 = indf.iloc[:,6]
-    columns.append(sample)
-    rawdf = pd.concat([rawdf, df1], axis=1)
-rawdf.columns = columns
+outfile1 = '{}_{}-samples_tpm.tsv'.format(args["outfile"], num_samples)
+outfile2 = '{}_{}-samples_raw.tsv'.format(args["outfile"], num_samples)
 if len(db) > 0:
     rawdf['Geneid'] = rawdf['Geneid'].map(db)
+
+# Write raw count data for all samples to file
 outdf = rawdf.drop('Length', axis=1)
 outdf.to_csv(outfile2, sep='\t', index=False, float_format='%.3f')
-tpmdf = rawdf.iloc[:,[0, 1]]
 
-for col_ind in range(2, len(files)+2):
+# Calculate TPM and write data for all samples to file
+columns = rawdf.columns
+tpmdf = rawdf.iloc[:,[0, 1]]
+for col_ind in range(2, num_samples+2):
     df1 = 1000 * rawdf.iloc[:,col_ind] / rawdf.iloc[:,1]
     df1 = 1000000 * df1 / sum(df1)
     tpmdf = pd.concat([tpmdf, df1], axis=1)
